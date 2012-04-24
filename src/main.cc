2215ecac72972f8dff2ebf5d4e6f5d698cad934f
Harald <fuvj42@gmail.com>
2012-04-24 21:43:16 +0200

    fix style

--- src/main.cc
+++ src/main.cc
@@ -5,50 +5,41 @@ using namespace std;
 
 class Vector2
 {
-public:
-	int x;
-	int y;
+  public:
+    int x;
+    int y;
 };
 
 class Actor
 {
-public:
-	Vector2 pos;
+  public:
+    Vector2 pos;
 };
 
 class AStarTile
 {
-public:	
-	Vector2 pos;
-	int G;
-	int H;
-	AStarTile *parentTile;
+  public:	
+    Vector2 pos;
+    int G;
+    int H;
+    AStarTile *parentTile;
 };
 
 
-bool map[10][10];
+bool map[10][10] = {0};
 
 int main(int argc, char** argv) {
-	  std::cout << "Hello, World!" << std::endl;
-	  for(int i = 0; i < 10; i++)
-	  {
-			for(int j = 0; j < 10; j++)
-			{
-				map[i][j] = false;
-			}
-	  }
+  vector<AStarTile> openFields;
 
-	  vector<AStarTile> openFields;
+  Actor actor;
+  actor.pos.x = 0;
+  actor.pos.y = 0;
+  bool done = false;
+  while(!done)
+  {
 
-	  Actor actor;
-	  actor.pos.x = 0;
-	  actor.pos.y = 0;
-	  bool done = false;
-	  while(!done)
-	  {
-		
-	  }
-	  fflush(stdin);
-	  getchar();
-	  return 0;
+  }
+  fflush(stdin);
+  getchar();
+  return 0;
 }
